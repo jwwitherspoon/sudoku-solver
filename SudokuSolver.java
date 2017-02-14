6e2f0198e60b513c791a5ffffa14782b38a8046e
jwwitherspoon <jwwitherspoon@gmail.com>
2017-02-14 17:08:36 -0500

    Added previously written files
    These were previously written in Eclipse.

---
+++ SudokuSolver.java
@@ -0,0 +1,40 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class SudokuSolver extends Application {
+	private Pane pane;
+	private GridPane grid;
+	private NumberBox[] row;
+	
+	@Override
+	public void start(Stage stage) {
+		
+	}
+	
+	public void solveRow() {
+		
+	}
+	
+	//TODO write check methods
+//	public boolean checkZone() {
+//		
+//	}
+//	
+//	public boolean checkColumn() {
+//		
+//	}
+//	
+//	public boolean checkRow() {
+//		
+//	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
