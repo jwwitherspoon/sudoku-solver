28c9633db14acc557c325ff562ff04f62d549972
jwwitherspoon <jwwitherspoon@gmail.com>
2017-03-21 17:01:25 -0400

    Update CrookSolver.java
    Changed class name to CrookSolver.
    Added better test puzzle and where to find the puzzle online.

--- CrookSolver.java
+++ CrookSolver.java
@@ -196,7 +196,7 @@ public class TextSolver {
 			}
 		}
 		
-		//TODO Add place-finding algorithm
+		//TODO Add preemptive set algorithm
 
 		//Print the solved puzzle or a message
 		if (puzzleSolved)
@@ -515,5 +515,15 @@ public class TextSolver {
 	
 }
 
-//Random test puzzle
-// 0 0 0 0 9 0 0 5 0 7 0 0 0 4 5 0 2 3 0 0 3 0 8 0 0 6 7 0 0 4 0 2 9 0 0 8 2 3 9 0 0 0 7 1 6 1 0 0 7 3 0 2 0 0 9 1 0 0 6 0 3 0 0 5 2 0 4 7 0 0 0 9 0 4 0 0 1 0 0 0 0
+//Test puzzle: WebSudoku easy puzzle 8,660,622,613
+//5 3 0 0 0 4 0 1 0 0 8 6 7 0 0 0 5 0 0 1 0 3 5 0 9 8 0 8 0 0 0 9 0 1 4 3 0 0 0 0 0 0 0 0 0 4 5 1 0 3 0 0 0 7 0 7 8 0 2 5 0 3 0 0 4 0 0 0 1 8 6 0 0 6 0 4 0 0 0 7 1
+//Solution
+//5 3 9 8 6 4 7 1 2 
+//2 8 6 7 1 9 3 5 4 
+//7 1 4 3 5 2 9 8 6 
+//8 2 7 5 9 6 1 4 3 
+//6 9 3 1 4 7 5 2 8 
+//4 5 1 2 3 8 6 9 7 
+//1 7 8 6 2 5 4 3 9 
+//3 4 2 9 7 1 8 6 5 
+//9 6 5 4 8 3 2 7 1
