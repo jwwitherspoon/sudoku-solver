e2b314303e6ee2d3b5bceae92f8b686eccb64f3e
jwwitherspoon <jwwitherspoon@gmail.com>
2018-09-27 00:32:10 -0400

    Update solve.js
    Updated data-pulling algorithm to match new grid format; added createPuzzle() method for test purposes

--- solve.js
+++ solve.js
@@ -1,52 +1,36 @@
 function solve() {
 	var field = createField();
 	var zone = createZone(field);
-
-	//If the word "test" is written in the text area, put a test puzzle in the box
-	//Used for testing purposes only
-	if (document.getElementById("input").value == "test") {
-		document.getElementById("input").value =
-			"5 3 0 0 0 4 0 1 0\n" +
-			"0 8 6 7 0 0 0 5 0\n" +
-			"0 1 0 3 5 0 9 8 0\n" +
-			"8 0 0 0 9 0 1 4 3\n" +
-			"0 0 0 0 0 0 0 0 0\n" +
-			"4 5 1 0 3 0 0 0 7\n" +
-			"0 7 8 0 2 5 0 3 0\n" +
-			"0 4 0 0 0 1 8 6 0\n" +
-			"0 6 0 4 0 0 0 7 1";
+	//Test puzzle functionality
+	if (document.getElementById("0-0").value == 't') {
+		createPuzzle();
 		return 0;
 	}
 
-	//If input matches correct syntax, create string representation of input
-	//Otherwise, print a message asking for the correct syntax
-	var text;
-	var pattern = /(?:(?:\d\s){8}\d\n){8}(?:\d\s){8}\d/;
-	if (pattern.test(document.getElementById("input").value)) {
-		//Create string representation of puzzle input
-		text = pattern.exec(document.getElementById("input").value)[0];
-	}
-	else {
-		document.getElementById("solution").innerHTML = "Please use the correct syntax when entering your puzzle.";
-		return 0;
-	}
-	
-	//Split puzzle into rows
-	var puzzle = text.split("\n");
-	//Split rows into values
-	for (var i=0; i<9; i++) {
-		puzzle[i] = puzzle[i].split(" ");
-	}
+	//Reset message board
+	document.getElementById("message").innerHTML = "";
+
 	//Add values to cells
 	for (var i=0; i<9; i++) {
 		for (var j=0; j<9; j++) {
-			field[i][j].value = parseInt(puzzle[i][j]);
-			//If the cell is given a value at the beginning or the puzzle, mark it solved
-			if (field[i][j].value != 0) {
+			//Get value of each cell
+			var temp = document.getElementById(i + "-" + j).value;
+
+			//If cell input is correct, fill cell with number.
+			if (temp.trim() == "") {
+				field[i][j].value = 0;
+			}
+			else if (temp > 0 && temp < 10) {
+				field[i][j].value = parseInt(temp);
 				field[i][j].solved = true;
+			}
+			else {
+				document.getElementById("message").innerHTML = "Please enter only values 1-9 in cells.";
+				return 0;
 			}
 		}
 	}
+
 	//Create enumeration of cells in one long line so as to scroll through them better in the solution algorithm
 	var enumeration = createEnumeration(field);
 
@@ -61,7 +45,7 @@ function solve() {
 		//Create two vars, one to store the current value and one to store the possible new value (starting with the next untested value)
 		var prev = current.value, next = current.value + 1;
 		//If the current cell is not tempsolved, try adding numbers into the cell until the entry does not violate the Sudoku condition or all values have been tested
-		while (!current.tempSolved && next<=9 && current.value==prev) {
+		while (!current.tempSolved && next <= 9 && current.value == prev) {
 			if (checkCell(current, next, field, zone)) {
 				current.value = next;
 				current.tempSolved = true;
@@ -71,7 +55,7 @@ function solve() {
 		//If the value does not violate the Sudoku condition
 		if (current.tempSolved) {
 			//If current cell is last cell, puzzle is solved
-			if (currentnum==enumeration.length-1) {
+			if (currentnum == enumeration.length - 1) {
 				puzzleSolved = true;
 			}
 			//If current cell is not last cell, continue solution with next cell as new current cell
@@ -82,7 +66,7 @@ function solve() {
 		//If all values violate the Sudoku condition
 		else {
 			//If current cell is first cell, puzzle has no solution
-			if (currentnum==0) {
+			if (currentnum == 0) {
 				impossible = true;
 			}
 			//If current cell is not first cell, erase value of current cell and continue solution with previous unsolved cell as new current cell
@@ -96,11 +80,23 @@ function solve() {
 
 	//Print puzzle
 	if (puzzleSolved) {
-		document.getElementById("solution").innerHTML = puzzleToText(field);
+		document.getElementById("message").innerHTML = puzzleToText(field);
 	}
 	else {
-		document.getElementById("solution").innerHTML = "This puzzle has no solution.";
+		document.getElementById("message").innerHTML = "This puzzle has no solution.";
 	}
+}
+
+//Function to create a puzzle in the grid for test purposes
+function createPuzzle() {
+	document.getElementById("0-0").value = 5; document.getElementById("0-1").value = 3; document.getElementById("0-5").value = 4; document.getElementById("0-7").value = 1;
+	document.getElementById("1-1").value = 8; document.getElementById("1-2").value = 6; document.getElementById("1-3").value = 7; document.getElementById("1-7").value = 5;
+	document.getElementById("2-1").value = 1; document.getElementById("2-3").value = 3; document.getElementById("2-4").value = 5; document.getElementById("2-6").value = 9; document.getElementById("2-7").value = 8;
+	document.getElementById("3-0").value = 8; document.getElementById("3-4").value = 9; document.getElementById("3-6").value = 1; document.getElementById("3-7").value = 4; document.getElementById("3-8").value = 3;
+	document.getElementById("5-0").value = 4; document.getElementById("5-1").value = 5; document.getElementById("5-2").value = 1; document.getElementById("5-4").value = 3; document.getElementById("5-8").value = 7;
+	document.getElementById("6-1").value = 7; document.getElementById("6-2").value = 8; document.getElementById("6-4").value = 2; document.getElementById("6-5").value = 5; document.getElementById("6-7").value = 3;
+	document.getElementById("7-1").value = 4; document.getElementById("7-5").value = 1; document.getElementById("7-6").value = 8; document.getElementById("7-7").value = 6;
+	document.getElementById("8-1").value = 6; document.getElementById("8-3").value = 4; document.getElementById("8-7").value = 7; document.getElementById("8-8").value = 1;
 }
 
 function checkCell(cell, value, field, zone) {
